7873615b6383040fdd67cc7f3c971ab8d9dbc93d
Roberto <robertorebolledo151@gmail.com>
2023-09-22 12:34:11 +0200

    finalización del ejercicio aun queda ponerlo bonito

--- caesar_chiper/index.js
+++ caesar_chiper/index.js
@@ -1,49 +1,62 @@
 $(document).ready(() => {
     var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var reverse = false;
 
     $('#encrypted').text('');
 
-
-    $('#descrypted, #number').on('input', function () {
-
+    function ci() {
         var word = $('#descrypted').val().toUpperCase();
-
         var number = parseInt($('#number').val());
 
         function cifrar(word, number) {
-
             var arr = word.split('');
-
             var result = [];
-
             arr.forEach(a => {
-
                 if (abc.indexOf(a) !== -1) {
-
                     var wordpos = abc.indexOf(a);
-
                     wordpos += number;
-
                     while (wordpos > abc.length) {
-
                         wordpos -= abc.length;
-
                     }
-
                     result.push(abc[wordpos]);
                 } else {
                     result.push(a);
                 }
-
-
             });
-
             return result.join('');
-
         }
 
-        $('#encrypted').text(cifrar(word, number));
+        function descrifrar(word, numer) {
+            var arr = word.split('');
+            var result = [];
+            arr.forEach(a => {
+                if (abc.indexOf(a) !== -1) {
+                    var wordpos = abc.indexOf(a);
+                    wordpos -= number;
+                    while (wordpos < 0) {
+                        wordpos += abc.length;
+                    }
+                    result.push(abc[wordpos]);
+                } else {
+                    result.push(a);
+                }
+            });
+            return result.join('');
+        }
 
+        if (reverse) {
+            $('#encrypted').text(descrifrar(word, number));
+
+        } else {
+            $('#encrypted').text(cifrar(word, number));
+        }
+
+    }
+
+    $(document).on('input', ci);
+
+    $(document).on('click', "#reverse", function () {
+        reverse = !reverse;
+        ci();
     });
-
 });
